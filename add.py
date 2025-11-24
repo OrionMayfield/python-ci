ed287c5316a40708acc8b9f43f03e9d5bdcdda13
Orion Mayfield (om3g25) <om3g25@soton.ac.uk>
2025-11-24 11:54:18 +0000

    added nonsense line

--- add.py
+++ add.py
@@ -2,6 +2,6 @@ def add(x: int, y: int) -> int:
     """Returns the sum of two integers."""
  #if not isinstance(x,int) or not isinstance(y,int):
   #   raise TypeError("Both parameters must be integers.")
-
+#nonsense line
  return x + y
 
